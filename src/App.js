4cd7e4e1a98754bd45228e386833e5cd6b592124
Léo Jansen <leo@leoo.app>
2022-09-06 18:55:22 -0300

    Css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Article, Brand, CTA, Feature, Navbar } from './components';
-import {Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers'
-
+import React from "react";
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3
+} from "./containers";
+import "./App.css";
 const App = () => {
   return (
-    <div className='App'>
-      <div className='gradient__bg'>
-        <Navbar/>
-        <Header/>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
       </div>
-      <Brand/>
-      <WhatGPT3/>
-      <Possibility/>
-      <Blog/>
-      <Footer/>
+      <Brand />
+      <WhatGPT3 />
+      <Possibility />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
