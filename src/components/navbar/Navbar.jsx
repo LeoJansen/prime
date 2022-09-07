59bf84462acd0470753385e15ded000fb4db5ddd
Léo Jansen <leo@leoo.app>
2022-09-07 20:26:49 -0300

    logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-import logo from "../../logo.svg";
+import React, {useState} from "react";
+import logo from "../../assets/logo.svg";
 
 import "./navbar.css";
 
+
+
+
+
 const Navbar = () => {
+  const [toogleMenu, setToogleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} />
         </div>
-        <div></div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign Up</button>
       </div>
     </div>
   );
