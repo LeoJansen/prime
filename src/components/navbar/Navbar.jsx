dd2591b0cc89a05beb8d2fc58c834f95e63dd9e3
Léo Jansen <leo@leoo.app>
2022-09-07 11:50:49 -0300

    Logo insert

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import logo from "../../logo.svg";
+
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo"></div>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} />
+        </div>
+        <div></div>
       </div>
     </div>
   );
