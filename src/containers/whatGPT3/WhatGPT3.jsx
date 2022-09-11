331a5d2152d3bbb58c92a70b6e87871c873dd3e4
Léo Jansen <leo@leoo.app>
2022-09-11 13:54:27 -0300

    Finish whatGPT3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Feature from '../../components/feature/Feature'
+import { texts } from './texts'
 import './whatgpt3.css'
 
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id="whpt3">
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title={texts.Title1} text={texts.Text1}/>
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className='gpt3__whatgpt3-container'>
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title={texts.Title2} text={texts.Text2}/>
+        <Feature title={texts.Title3} text={texts.Text3}/>
+        <Feature title={texts.Title4} text={texts.Text4}/>
       </div>
     </div>
   )
---
+++ src/containers/whatGPT3/texts.js
@@ -0,0 +1,11 @@
+export const texts = {
+    Title1: "What is GPT-3",
+    Text1:"We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.",
+    Title2: "Chatbots",
+    Text2: "We so opinion friends me message as delight. Whole front do of plate heard oh ought. ",
+    Title3: "Knowledgebase",
+    Text3: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
+    Title4: "Education",
+    Text4: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+
+}
