b0326da0d3e98e5b0a125c07c592ad2edeb65a37
Léo Jansen <leo@leoo.app>
2022-09-12 19:12:03 -0300

    Blog first commit

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text"> We are blogging about it.</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
