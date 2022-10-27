bb984aae99b095dedd8edaca4326586c07bcd374
Léo Jansen <leo@leoo.app>
2022-10-27 23:41:05 +0000

    Update 3 files

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -10,9 +10,18 @@ const CTA = () => {
         <h3>Acesse nossas redes sociais </h3>
       </div>
       <div className="gpt3__cta-btn">
-        <img src={facebook}/>
-        <img src={instagram}/>
-        <img src={whats}/>
+        <div className="instagram">
+            <a href="https://instagram.com/prime.ret?igshid=YmMyMTA2M2Y="><img src={instagram}/></a>
+        </div>
+      <div className="whatsapp">
+      <a href="https://wa.me/5521979761500"><img src={whats}/></a>
+      </div>
+      <div className="facebook">
+        <a href="https://www.facebook.com/prime.ret/"><img src={facebook}/></a>
+      </div>
+      
+        
+        
         
       </div>
     </div>
