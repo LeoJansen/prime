bb2d8acbacb35277979f99755f89306842a53838
Léo Jansen <leo@leoo.app>
2022-10-31 16:19:54 -0300

    adj

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -8,19 +8,19 @@ const CTA = () => {
     <motion.div className="gpt3__cta" id="cta"
     whileInView={{ opacity: [0, 1] }}
     transition={{ duration: 0.5 }}>
-      <div className="gpt3__cta-content">
+      <div className="content">
         
         <h3>Acesse nossas redes sociais </h3>
       </div>
-      <div className="gpt3__cta-btn">
+      <div className="btns">
         <div className="instagram">
-            <a href="https://instagram.com/prime.ret?igshid=YmMyMTA2M2Y="><img src={instagram}/></a>
+            <a href="https://instagram.com/prime.ret?igshid=YmMyMTA2M2Y=" target="_blank"><img src={instagram}/></a>
         </div>
       <div className="whatsapp">
-      <a href="https://wa.me/5521979761500"><img src={whats}/></a>
+      <a href="https://wa.me/5521979761500" target="_blank"><img src={whats}/></a>
       </div>
       <div className="facebook">
-        <a href="https://www.facebook.com/prime.ret/"><img src={facebook}/></a>
+        <a href="https://www.facebook.com/prime.ret/" target="_blank"><img src={facebook}/></a>
       </div>
       
         
